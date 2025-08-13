077fa085f9f77d72ce8c0329b7686283327ed56d
LauM05 <laureanomora2005@gmail.com>
2025-08-12 22:38:30 -0300

    Update IMC_Laureano Mora.c

--- IMC_Laureano Mora.c
+++ IMC_Laureano Mora.c
@@ -3,14 +3,20 @@
 int main(int argc, char *argv[]) {
 	
 float peso=0, altura=0, imc=0;	
-
+do{
 printf("ingrese su peso:");
 scanf("%f", &peso);
-
+	if(peso <= 0){ 
+		printf("ERROR: Peso negativo\n");}
+}while (peso <= 0);
+do{
 printf("ingrese su altura (m):");
 scanf("%f", &altura);
+	if(altura <= 0){
+		printf("ERROR: Altura negativa\n");}
+}while(altura <= 0);
 
-imc= peso/(altura*altura);
+	imc= peso/(altura*altura);
 
 printf("su indice de masa corporal es: %.2f\n",imc);
 
